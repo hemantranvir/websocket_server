b95edb4bf3c994db3956381be32eaeab2ff802cc
Hemant Ranvir <hemantranvir@gmail.com>
2018-12-21 22:20:46 +0900

    Setting port as program argument

--- src/server_main.cpp
+++ src/server_main.cpp
@@ -6,11 +6,29 @@
 
 int main(int argc, char** argv)
 {
-    int server_port = 32000;
-    WebSocketServer server_app(server_port, std::cout);
+    int pos = 1;
+    int port = 32000;
+    while (pos < argc) {
+        std::string cur_arg(argv[pos]);
+        if (std::string(argv[pos]) == "-i") {
+            try {
+                port = std::stoi(argv[++pos]);
+            }
+            catch (...) {
+                std::cout << "Error: Invalid port value" << std::endl;
+                return -1;
+            }
+	} else {
+            std::cout << "Error: unknown option: " << argv[pos] << std::endl;
+            return -1;
+        }
+        ++pos;
+    }
+
+    WebSocketServer server_app(port, std::cout);
     server_app.SetMaxThreads(64);
     if (!server_app.Initialize()) {
-        std::cout << "Failed to initialize server." << std::endl;
+        std::cout << "Error: Failed to initialize server." << std::endl;
         return -1;
     }
     server_app.Run();
@@ -21,6 +39,8 @@ int main(int argc, char** argv)
         std::cout << "To Terminate Server Operation Enter \"exit\"" << std::endl;
         std::cin >> input;
     } while (input != "exit");
+
+    std::cout << "...exiting" << std::endl;
 
     //server_app.StopAll(true);
 }
